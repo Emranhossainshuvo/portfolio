506805356c818c761f21586934f8f10f4ab001c8
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-02-25 18:08:25 +0600

    changed the whole design of the navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                <p className="tracking-tighter text-2xl font-semibold">Md. Emran Hossain</p>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
