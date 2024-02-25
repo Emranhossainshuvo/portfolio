d614a796f10e69d5cace08c086d941e99689d472
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-02-25 21:59:14 +0600

    added the basic toggle icons and added onclick change function to them

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
-
+import { useState } from "react";
 const Navbar = () => {
+
+    const [theme, setTheme] = useState(false);
+
+    const handleTheme = () => {
+        setTheme(!theme); 
+    }
+
     return (
         <>
-            <div>
-                <p className="tracking-tighter text-2xl font-semibold">Md. Emran Hossain</p>
+            <div className="flex mt-5 mx-5 justify-between">
+                <div>
+                    <p className="tracking-tighter text-2xl font-semibold">
+                        Md. Emran Hossain
+                    </p>
+                </div>
+                <div>
+                    <span onClick={handleTheme} className=" w-4">{theme ? <i className="fa-regular fa-sun"></i>: <i className="fa-regular fa-moon"></i>}</span>
+                </div>
             </div>
         </>
     );
